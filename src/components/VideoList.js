be48edc74850f48a05c207e117a68ac330091e22
E-rick <erickmartinezjunior@gmail.com>
2019-03-29 17:17:31 -0400

    Add video item component

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
 	const renderedList = videos.map(video => {
-		return <VideoItem />;
+		return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
 	});
-	return <div>{renderedList}</div>;
+	return <div className='ui relaxed divided list'>{renderedList}</div>;
 };
 
 export default VideoList;
