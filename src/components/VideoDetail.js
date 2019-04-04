0ce13cc727f932d520c27674a101b5a1493dd804
E-rick <erickmartinezjunior@gmail.com>
2019-04-04 15:37:59 -0400

    Add video publish date

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './VideoDetail.css';
+import Moment from 'react-moment';
 
 const VideoDetail = ({ video }) => {
 	if (!video) {
@@ -6,14 +8,19 @@ const VideoDetail = ({ video }) => {
 	}
 
 	const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+	const videoPublishDate = video.snippet.publishedAt;
 
 	return (
 		<div>
 			<div className='ui embed'>
 				<iframe src={videoSrc} frameBorder='0' title='video player' />
 			</div>
-			<div className='ui segment'>
-				<h4 className='ui header'>{video.snippet.title}</h4>
+			<div className='video-detail ui segment'>
+				<h4 className='video-title ui header'>{video.snippet.title}</h4>
+				<span className='video-date'>
+					Published on <Moment format='D MMM, YYYY' date={videoPublishDate} />
+				</span>
+				<hr />
 				<p>{video.snippet.description}</p>
 			</div>
 		</div>
